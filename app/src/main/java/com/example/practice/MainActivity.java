6f022d5312673a8860f9ba2e1274c30482005147
Gurpreet Singh <fxgurpreet@gmail.com>
2022-02-17 00:24:14 -0500

    Initital commit

--- app/src/main/java/com/example/practice/MainActivity.java
+++ app/src/main/java/com/example/practice/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("Gurpreet is a bad driver");
+        System.out.println("Jeffrey is a good driver and he drives a lexus");
     }
 }
